b50520727bc0966aece9e7472377bde618577df1
Trevor Claridge <trevclaridge@gmail.com>
2025-06-18 13:19:01 -0400

    try filenameFormat for img names

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,12 @@ const markdownItMark = require("markdown-it-mark");
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+        filenameFormat: function (id, src, width, format, options) {
+            return `${id}-${width}.${format}`
+        },
+    });
+
     eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItMark));
 
 
