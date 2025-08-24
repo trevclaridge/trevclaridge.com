4d87b51938f2dd6528ffa58dedc60274dd8b8f1b
Trevor Claridge <trevclaridge@gmail.com>
2025-08-24 17:21:10 -0400

    add writ of rulers link

--- src/script.js
+++ src/script.js
@@ -1,7 +1,29 @@
+const addWritOfRulersDownloadLink = async () => {
+    let url = "https://api.github.com/repos/trevclaridge/Writ-of-Rulers/releases/latest";
+
+    const response = await fetch(url);
+    const myJson = await response.json();
+
+    const link = myJson["assets"][0]["browser_download_url"];
+    const element = document.getElementById("writ-of-rulers-download");
+    element.href = link;
+}
+
 // if a list element is shorter than 70 characters, we shrink the margin by giving it an additional class
-var elements = document.getElementsByTagName('li')
-for (var i = 0; i < elements.length; i++) {
-    if (elements[i].textContent.length < 70) {
-        elements[i].classList.add("article-content-list")
+const fixListElementSpacing = () => {
+    var elements = document.getElementsByTagName('li')
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].textContent.length < 70) {
+            elements[i].classList.add("article-content-list")
+        }
     }
 }
+
+let path = window.location.pathname;
+if (path == '/writ/') {
+    addWritOfRulersDownloadLink();
+}
+else if (path.includes("/posts/")) {
+    fixListElementSpacing();
+}
+
